04ac461626ca459a3ba0f5d7c0014c95b06029dc
tilakpatidar <tilakpatidar@gmail.com>
2015-11-24 20:13:08 +0530

    rewrite lib_test.js

--- lib_test.js
+++ lib_test.js
@@ -1,7 +1,7 @@
 var bot=require('./index.js').init();
 
 //Displays json config of the crawler
-//console.log(bot.getConfig());
+console.log(bot.getConfig());
 
 //insert a url to crawl
 bot.insertSeed("http://www.geeksforgeeks.org/","nutch",false);
@@ -9,4 +9,60 @@ bot.insertSeed("http://www.geeksforgeeks.org/","nutch",false);
 //remove url
 bot.removeSeed("http://www.geeksforgeeks.org/");
 
-bot.set("mongodb",{"mongodb_uri": "mongodb://192.168.101.5:27017/crawl1","mongodb_collection": "10",});
+bot.set("headers",{"User-Agent":"Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",
+		"Accept-Language":"en-us,en-gb,en;"
+	});
+
+//clearing the crawl list
+bot.clearSeed();
+
+//to reset the crawler
+bot.reset(function(){
+
+
+	//loading a seed file
+	//format of seed file
+	//tuples of <url>\t<parseFileName>\t<phantomjs_rendering(true/false)>
+	//seprated by new lines
+
+	//ex http://www.google.com	nutch	false
+	//   http://www.yahoo.com	nutch	true
+
+	bot.loadSeedFile("/home/bot/Desktop/seed");
+
+	//check if url already present
+
+	console.log(bot.isSeedPresent());
+
+	//use get and set to modify config
+
+	console.log(bot.get("bot_name"));
+
+	bot.set("bot_name","zaphod");
+
+	bot.set("robot_agent","*");
+
+	
+
+	//to verify a property exists
+
+	console.log(bot.isProperty("bot_name"));
+
+	bot.crawl();
+
+
+	/* some test cases for tika testing
+
+	bot.insertSeed("https://training.github.com/kit/downloads/github-git-cheat-sheet.pdf","nutch",false);	
+	bot.insertSeed("http://ra.adm.cs.cmu.edu/anon/anon/isri2005/CMU-ISRI-05-100.pdf","nutch",false);	
+	bot.insertSeed("http://ra.adm.cs.cmu.edu/anon/anon/isri2005/CMU-ISRI-05-104.pdf","nutch",false);	
+	bot.insertSeed("http://ra.adm.cs.cmu.edu/anon/anon/isri2005/CMU-ISRI-05-116.pdf","nutch",false);	
+	bot.insertSeed("http://ra.adm.cs.cmu.edu/anon/anon/isri2005/CMU-ISRI-05-121.pdf","nutch",false);	
+	bot.insertSeed("http://ra.adm.cs.cmu.edu/anon/anon/isri2005/CMU-ISRI-05-132.pdf","nutch",false);
+
+	*/
+
+
+
+
+});
