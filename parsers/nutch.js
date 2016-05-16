f3153b340c5c6dd657042d22bec5bd868f0af995
Tilak Patidar <tilakpatidar@gmail.com>
2016-05-16 17:43:42 +0530

    added more meta tags to parser

--- parsers/nutch.js
+++ parsers/nutch.js
@@ -1,9 +1,24 @@
 var cheerio = require('cheerio');
 var parent_dir=process.getAbsolutePath(__dirname);
 var config=process.bot_config;
+var check = require('check-types');
+
+
+function fetchMultipleAttr($(selector, attribute){
+
+	var list = [];
+	$(selector).each(function (index, element) {
+	  list.push($(element).attr(attribute));
+	});
+
+	return list;
+
+}
+
 var app={
 	"parse":function(data,url){
 		var indexed={};
+		var isWebPage = false;
 		//console.log(data);
 		if(data===undefined){
 			data="";
@@ -12,15 +27,18 @@ var app={
 		if(config.getConfig("tika")){
 			if(url.match(config.getConfig("tika_supported_files"))!==null){
 				//console.log('web0');
+				isWebPage = false;
 				indexed=app.parseDocument(data,url);
 			}
 			else{
 				//console.log('web1');
+				isWebPage = true;
 				indexed=app.parseWebPage(data,url);
 			}
 		}
 		else{
 			//console.log('web2');
+			isWebPage = true;
 			indexed=app.parseWebPage(data,url);
 		}
 
@@ -46,6 +64,14 @@ var app={
          dic._source["mime"]=indexed["mime"];
          dic._source["title"]=indexed["title"];
          dic._source["file_info"]=indexed["file_info"];
+
+         if(isWebPage){
+         	dic._source["twitter"] = indexed["twitter"];
+         	dic._source["author"] = indexed["author"];
+         	dic._source["open_graph"] = indexed["open_graph"];
+         	dic._source["rss_feeds"] = indexed["rss_feeds"];
+         }
+
 		 return [indexed["dom"],dic,[], indexed["html"]];//cheerio object ,dic to insert ,inlinks to give
 		 //for documents indexed["dom"] is null
 		 //then raw html content
@@ -90,6 +116,77 @@ var app={
 		 ret["host"]=ret["output"][1];
 		 ret["meta_keywords"]=$('meta[name="keywords"]').attr('content');
 		 ret["description"]=$('meta[name="description"]').attr('content');
+
+		 //get page author
+
+		 ret["author"] = $('link[rel="author"]').attr('href');
+
+		 if(!check.assigned(ret["author"])){
+		 	ret["author"] = $('a[rel="author"]').attr('href');
+		 }
+		 if(!check.assigned(ret["author"])){
+		 	ret["author"] = $('a[rel="me"]').attr('href');
+		 }
+		 if(!check.assigned(ret["author"])){
+		 	ret["author"] = $('link[rel="publisher"]').attr('href');
+		 }
+
+		 if(!check.assigned(ret["author"])){
+		 	delete ret["author"];
+		 }
+
+		 //facebook open graph data
+
+		 ret['open_graph'] = {};
+
+		 ret['open_graph']['title'] = fetchMultipleAttr($('meta[property="og:title"]'),'content');
+		 ret['open_graph']['type'] = fetchMultipleAttr($('meta[property="og:type"]'),'content');
+		 ret['open_graph']['image'] = fetchMultipleAttr($('meta[property="og:image"]'),'content');
+		 ret['open_graph']['url'] = fetchMultipleAttr($('meta[property="og:url"]'),'content');
+		 ret['open_graph']['description'] = fetchMultipleAttr($('meta[property="og:description"]'),'content');
+
+		 for(var key in ret['open_graph']){
+		 	if(!check.assigned(ret['open_graph'][key])){
+		 		delete ret['open_graph'][key];
+		 	}
+		 }
+
+
+		 //twitter card
+
+		 ret['twitter'] = {};
+
+		 ret['twitter']['title'] = fetchMultipleAttr($('meta[name="twitter:title"]'),'content');
+		 ret['twitter']['card'] =fetchMultipleAttr($('meta[name="twitter:card"]'),'content');
+		 ret['twitter']['image'] = fetchMultipleAttr($('meta[name="twitter:image"]'),'content');
+		 ret['twitter']['image_alt'] = fetchMultipleAttr($('meta[name="twitter:image:alt"]'),'content');
+		 ret['twitter']['description'] = fetchMultipleAttr($('meta[name="twitter:description"]'),'content');
+		 ret['twitter']['domain'] = fetchMultipleAttr($('meta[name="twitter:domain"]'),'content');
+		 ret['twitter']['creator_id'] = fetchMultipleAttr($('meta[name="twitter:creator:id"]'),'content');
+		 ret['twitter']['creator_username'] = fetchMultipleAttr($('meta[name="twitter:creator"]'),'content');
+		 ret['twitter']['site_username'] = fetchMultipleAttr($('meta[name="twitter:site"]'),'content');
+		 ret['twitter']['site_id'] = fetchMultipleAttr($('meta[name="twitter:site:id"]'),'content');
+
+
+		 //gather rss feeds from pages
+
+		 ret['rss_feeds'] = fetchMultipleAttr($('link[rel="alternate"][type="application/rss+xml"]'),'href');
+
+
+
+
+		 for(var key in ret['twitter']){
+		 	if(!check.assigned(ret['twitter'][key])){
+		 		delete ret['twitter'][key];
+		 	}
+		 }
+		 
+		 for(var index in ret['rss_feeds']){
+		 	if(!check.assigned(ret['rss_feeds'][index])){
+		 		delete ret['rss_feeds'][index];
+		 	}
+		 }
+
 		 if(ret["description"]===undefined || ret["description"]===""){
 		 	ret["description"]="";
 		 }
