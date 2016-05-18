f06bca3383a8c26aeefedd775aa42762668c266d
Tilak Patidar <tilakpatidar@gmail.com>
2016-05-18 22:18:44 +0530

    fixed undefined error in ui

--- server/my_code/scripts.js
+++ server/my_code/scripts.js
@@ -328,17 +328,11 @@
 				      if(Object.keys(js)!==0){
 				      	for (var i = 0; i < js.length; i++) {
 				      		var obj=js[i];
-				      		if(obj['data']['_source']){
+				      		try{
+				      			var title=obj['data']['_source']['title'];
+				      		}catch(err){
 				      			var title = "Untitled Title";
-				      		}else{
-
-
-					      		try{
-					      			var title=obj['data']['_source']['title'];
-					      		}catch(err){
-					      			var title=obj['data'][1]['_source']['meta_description'].substring(0,100);
-					      		}
-					      	}
+				      		}
 				      		var dum=rowGenerator(obj,TABLES[1][3],"page");
 				      		fin=fin.add(dum);
 				      	};
