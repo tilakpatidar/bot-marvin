a915415cee6f7f642086b73adfd0a7318eb2c86e
Tilak Patidar <tilakpatidar@gmail.com>
2016-07-13 22:37:59 +0530

    added man pages back

--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 jsdoc -p -c ./config/doc_conf.json --verbose --destination ./docs/
+mv ./docs/ ../
+git checkout gh-pages
+cp -rp ../docs/* ./
+git add -A
+git commit -m "docs updated"
+git push origin gh-pages
+git checkout master
