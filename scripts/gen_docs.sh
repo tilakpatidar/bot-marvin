bde04fb14bbd9b95567750e95ea0498f7544c44f
Tilak Patidar <tilakpatidar@gmail.com>
2016-07-13 22:35:18 +0530

    updated scripts and gitignore

--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
 jsdoc -p -c ./config/doc_conf.json --verbose --destination ./docs/
-mv ./docs/ ../
-git checkout gh-pages
-cp -rp ../docs/* ./
-git add -A
-git commit -m "docs updated"
-git push origin gh-pages
-git checkout master
