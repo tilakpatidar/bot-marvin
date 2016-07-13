5b08fb6ca72e9834d3704a0c6588d04c9fe0106b
Tilak Patidar <tilakpatidar@gmail.com>
2016-07-13 23:38:29 +0530

    fetch buckets according to domain group in queued fashion

--- setup_crawler.sh
+++ setup_crawler.sh
@@ -6,4 +6,4 @@ sudo nodejs lib/db_config.js --force &&
 echo "sudo nodejs index.js --load-config ./config.json --force " &&
 sudo nodejs index.js --load-config ./config.json --force &&
 echo "sudo nodejs index.js --loadSeedFile seedf.json --force" &&
-sudo nodejs index.js --loadSeedFile seed.json --force 
+sudo nodejs index.js --loadSeedFile seedf.json --force 
