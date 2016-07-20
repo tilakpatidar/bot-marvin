ec8408f5a8c302c856cb8c421de5d4ff3fa574bd
Tilak Patidar <tilakpatidar@gmail.com>
2016-07-20 18:31:00 +0530

    small fixes

--- setup_crawler.sh
+++ setup_crawler.sh
@@ -3,7 +3,7 @@ echo "sudo nodejs index.js --reset --force" &&
 #sudo nodejs index.js --reset --force &&
 echo "sudo nodejs lib/db_config.js --force" &&
 sudo nodejs lib/db_config.js --force &&
-echo "sudo nodejs index.js --load-config ./config.json --force " &&
-sudo nodejs index.js --load-config ./config.json --force &&
-echo "sudo nodejs index.js --loadSeedFile seedf.json --force" &&
+echo "sudo nodejs index.js --config --force " &&
+sudo nodejs index.js --config --force &&
+echo "sudo nodejs index.js --loadSeedFile seed.json --force" &&
 sudo nodejs index.js --loadSeedFile seed.json --force 
