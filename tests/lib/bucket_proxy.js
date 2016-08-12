a6d1d3be7f79c871986d28be582e9a48e92c1049
Tilak Patidar <tilakpatidar@gmail.com>
2016-08-12 20:03:25 +0530

    new tests and few fixes

--- tests/lib/bucket_proxy.js
+++ tests/lib/bucket_proxy.js
@@ -8,14 +8,13 @@ describe('Testing Bucket Proxy', function() {
     	var b = new bucket(1000);
     	var arr = [];
     	var yes =[];
-    	var no = [];
     	for (var i = 0; i < 1200; i++) {
     		var url = {url: "http://www.google.com/"+i, domain: "http://www.google.com", level: 1};
     		var status = b.pushURL(url);
-    		status ? yes.push(status) : no.push(status);
+    		if(status) yes.push(status);
     	};
-
-    	assert(no.length === 200 && yes.length === 1000, "cache limit not working");
+        
+    	assert(yes.length === 1200, "cache limit and reallocation not working");
 
     });
 
